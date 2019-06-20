e08628dd6d756723d0036d711bcd20f41a169d13
benpankow <pankow.ben@gmail.com>
2019-06-20 21:01:33 +0000

    Drop support for python 3.3

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
